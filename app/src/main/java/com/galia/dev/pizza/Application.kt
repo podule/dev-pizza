b849071d0e77f732a734c1de4d6ffd86bd6e215d
g.karasheva <galia.k@list.ru>
2022-09-22 16:31:07 +0300

    Dev-pizza - приложение заказа пиццы
    - добавлена библиотека внедрения зависимостей Hilt
    - Подключен view binding

---
+++ app/src/main/java/com/galia/dev/pizza/Application.kt
@@ -0,0 +1,7 @@
+package com.galia.dev.pizza
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application: android.app.Application() {
+}
--- app/src/main/java/com/galia/dev/pizza/menu/MenuFragment.kt
+++ app/src/main/java/com/galia/dev/pizza/menu/MenuFragment.kt
@@ -5,18 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.galia.dev.pizza.R
+import androidx.fragment.app.viewModels
+import com.galia.dev.pizza.databinding.FragmentMenuBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MenuFragment : Fragment() {
 
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MenuViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
